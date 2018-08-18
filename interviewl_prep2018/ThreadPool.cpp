32416917f4f11406ce8346b61a49906d3289d135
Corbin Charpentier <corbin.charpentier@gmail.com>
2018-08-17 20:11:45 -0700

    Push along implementation of two thread pool

--- interviewl_prep2018/ThreadPool.cpp
+++ interviewl_prep2018/ThreadPool.cpp
@@ -3,73 +3,109 @@
 #include <queue>
 #include <memory>
 #include <vector>
+#include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
 class Runnable {
+public:
     virtual void execute() = 0;
+    // TODO: delete constructurs
 };
 
 class TestClass : Runnable {
 public:
-    int id;
+    static int id = 0;
     void execute() {
-        cout << "Hello from runnable: " << id << endl;
+        cout << "Hello from runnable: " << id++ << endl;
     }
 };
 
+// Designed to be accessed by a single thread 
+// TODO: make multi-threaded access possible
 class ThreadPool {
 public:
     ThreadPool() {
     }
 
-    ThreadPool(const int n) : numThreads(n) {
-        
+    ThreadPool(const int n) 
+    : numThreads(n) 
+    , workerMutexes(vector<mutex>(n)) 
+    , workerCvs(vector<condition_variable>(n)) 
+    {
         this->coordinator = thread(this->coordinate);
 
         for(int i = 0; i < numThreads; i++) {
-            workers.push_back(thread(this->work));
-            workMappings.push_back(nullptr);
+            workers.push_back(thread(this->work, this, i));
         }
     }
 
     void queueWork(shared_ptr<Runnable> r) {
-        qRunnables.push(r);
-        // signal coordinator
+        lock_guard guard(queueMutex);
+        coordCv.notify_all();
     }
 
 private:
     void coordinate() {
         while(1) {
-            // wait for signal that we have work
-            if(qThreads.empty()) {
-                // wait for signal from finished thread
-            } else {
-                int threadId; // = something
-                auto runnable = qRunnables.front();
-                qRunnables.pop();
+            std::unique_lock<std::mutex> lck(queueMutex);
+            coordCv.wait(lck);
 
-                workMappings[threadId] = runnable;
-                // signal thread to continue
-            }
+            int threadId;
+            shared_ptr<Runnable> runnable;
+
+            runnable = runnableQueue.front();
+            runnableQueue.pop();
+
+            workerCvs[threadId].notify_all();
         }
     }
 
-    void work(shared_ptr<Runnable> runnable) {
+    void work(const int id) {
+        while(1) {
+            std::unique_lock<std::mutex> lck(workerMutexes[id]);
+            workerCvs[id].wait(lck);
 
+            auto runnable = queuePop();
+            runnable->execute();
+        }
     }
 
+    ~ThreadPool() {
+        for(int i = 0; i < workers.size(); i++) {
+            workers[i].join();
+        }
+    }
 private:
-    queue<thread> qThreads;
-    queue<shared_ptr<Runnable>> qRunnables;;
+    shared_ptr<Runnable> queuePop() {
+        lock_guard guard(queueMutex);
+        auto runnable = runnableQueue.front();
+        runnableQueue.pop();
+        return runnable;
+    }
+    void queuePush(shared_ptr<Runnable> runnable) {
+        lock_guard guard(queueMutex);
+        runnableQueue.push(runnable);
+    }  
+private:
+
+    condition_variable coordCv;
+    mutex queueMutex;
+
+    queue<shared_ptr<Runnable>> runnableQueue;
 
     int numThreads;
     thread coordinator;
     vector<thread> workers;
-    vector<shared_ptr<Runnable>> workMappings;
+    vector<condition_variable> workerCvs;
+    vector<mutex> workerMutexes;
 };
 
 int main() {
     ThreadPool tp(5);
+
+    // wait for a few seconds
+    
     return 0;
 }
